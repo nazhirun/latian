2c0af3c56dae0ec53533befa716e630518ba883e
Nazhirun <najheeeer@gmail.com>
2024-08-13 10:36:04 +0700

    ganti koneksi

--- latuas/koneksi.php
+++ latuas/koneksi.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
     $username = "root";
-    $password = "";
-    $database = "db_uas";
+    $password = "A123b142";
+    $database = "u710910476_dbuas";
     $connect = mysqli_connect($host, $username, $password, $database);
     mysqli_select_db($connect, $database) or Die ("Koneksi Gagal Ke Database!");
 ?>
